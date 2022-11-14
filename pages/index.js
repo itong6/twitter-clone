3f75bc1dece0e1bd8e1e0b1ebc54d34dc9dfd54d
Trevor Lee <tlee297@my.bcit.ca>
2022-11-14 15:57:53 -0800

    sign up with email and password

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { auth } from '../firebase/firebase.config';
+import { useState } from "react";
 
 export default function Home() {
+  const [registerEmail, setRegisterEmail] = useState('')
+  const [registerPassword, setRegisterPassword] = useState('')
+
+  const register = async () => {
+    try {
+      setRegisterEmail('')
+      setRegisterPassword('')
+      const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword)
+      console.log(user)
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
 
   const GoogleSignIn = async () => {
     const provider = new GoogleAuthProvider()
@@ -13,7 +27,33 @@ export default function Home() {
 
   return (
     <div>
-      <button onClick={() => GoogleSignIn()}>Log in with Google</button>
+      <div>
+        <form>
+          <h2>Register</h2>
+          <div>
+            <div>
+              <label>Email</label>
+              <input
+                placeholder='Email...'
+                onChange={(event) => {
+                  setRegisterEmail(event.target.value)
+                }}
+              />
+            </div>
+            <div>
+              <label>Password</label>
+              <input
+                placeholder='Password...'
+                onChange={(event) => {
+                  setRegisterPassword(event.target.value)
+                }}
+              />
+            </div>
+            <button onClick={register}>Register User</button>
+            <button onClick={() => GoogleSignIn()}>Log in with Google</button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
