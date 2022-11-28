411ba4e519985127e22563e9c4a18bc48dce28ef
atran02 <atran65@my.bcit.ca>
2022-11-28 01:15:38 -0800

    upload tweets on screen and add route btn

--- pages/tweets.js
+++ pages/tweets.js
@@ -2,14 +2,27 @@ import React from "react";
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, GoogleAuthProvider } from "firebase/auth";
 import { auth, db } from '../firebase/firebase.config';
 import { useState } from "react";
-import { Firestore } from "firebase/firestore";
-import { collection, doc, addDoc } from "firebase/firestore";
+import { Firestore, getDocs } from "firebase/firestore";
+import { collection, doc, addDoc} from "firebase/firestore";
 import { async } from "@firebase/util";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function Tweets(){
+  
+    const router = useRouter();
+    const handleClick = (e) => {
+      router.back('./')
+    }
+  
+
+    useEffect(()=>{
+        showTweet()
+    },[])
 
     const [user, setUser] = useState({})
     const [tweet, setTweet] = useState('')
+    const [newTweet, showNewTweet] = useState([])
 
     const handleSubmit = event =>{
         event.preventDefault()
@@ -23,21 +36,51 @@ export default function Tweets(){
             userId: auth.currentUser.uid
         });
         // console.log('u suckkkk')
+        showTweet();
+    }
+
+    const showTweet = async()=>{
+        const getDoc = await getDocs(collection(db, "posts"));
+        var newTweets = [];
+        getDoc.forEach((doc)=>{
+            console.log(doc.id, "=>", doc.data());
+            newTweets.push({...doc.data(),id:doc.id})
+        });
+        showNewTweet([...newTweets]);
+        console.log(newTweet);
     }
     
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <label>{user.email}</label>
-                <input 
-                    placeholder="What's happening?"
-                    type="text"
-                    onChange={(event)=> {
-                        setTweet(event.target.value)
-                    }}
-                />
-                <button onClick={createTweet}>Tweet</button>
-            </form>
+            <button onClick={()=>handleClick()}>home</button>
+            <div>
+                <form onSubmit={handleSubmit}>
+
+                    {/* helppp can someone make the user name/email show like on the home page when logged in pls */}
+                    <div>{user.email}</div>
+                    <input 
+                        placeholder="What's happening?"
+                        type="text"
+                        onChange={(event)=> {
+                            setTweet(event.target.value)
+                        }}
+                    />
+                    <button onClick={createTweet}>Tweet</button>
+                </form>
+            </div>
+
+            {/* show tweets */}
+            
+            <div>
+                {/* <button onClick={()=>showTweet()}>test button</button> */}
+                {newTweet.map(tweet =>{
+                    return(
+                        <div key={tweet.id}>
+                            <p>{tweet.text}</p>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
